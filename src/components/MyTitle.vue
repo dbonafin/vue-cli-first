61186e8fed2653b8ef69c6989cf3cfb34edf304d
dbonafin <demianbonafina@gmail.com>
2022-07-05 18:08:05 +0200

    title done

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> {{msg}} </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MyTitle",
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+h1 {
+    color: brown;
+}
+
+</style>
